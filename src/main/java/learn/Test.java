330bdb9d9be53ff1abfbaae39886f3e23422758e
mganapatvamsi1 <ganapathiraju.vamsi@gmail.com>
2020-05-02 18:01:59 -0500

    pom changes
    pom changes

--- src/main/java/learn/Test.java
+++ src/main/java/learn/Test.java
@@ -1,4 +1,14 @@
 package learn;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
 public class Test {
+
+    public void getTesting() {
+
+    }
+
 }
--- src/main/java/learn/javaspring/HelloMaven.java
+++ src/main/java/learn/javaspring/HelloMaven.java
@@ -1,9 +1,20 @@
 package learn.javaspring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Slf4j
 public class HelloMaven {
 
+//    public final static Logger log = LoggerFactory.getLogger(HelloMaven.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello Maven") ;
+        log.error("error logs");
+        log.warn("testing warn logs");
+        log.info("hey i started logging finally");
+        log.debug("hello maven ");
+//        System.out.println("Hello Maven") ;
     }
 
 }
